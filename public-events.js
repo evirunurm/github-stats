24233d4895a526e92176821a609a7836141cbf40
Evelin <teakid21@gmail.com>
2022-02-27 15:16:36 +0100

    requests pushes from last 90 days, pagination in progress

--- public-events.js
+++ public-events.js
@@ -1,19 +1,25 @@
 const https = require("https");
+const querystring = require('querystring');
+
+const baseUrl = '/users/evirunurm/events/public?per_page=100'
+
+
 
 const options = {
 	hostname: 'api.github.com',
-	path: '/users/evirunurm/events/public',
+	path: '/users/evirunurm/events/public?per_page=100',
 	method: 'GET',
 	headers: {
-		'user-agent': 'node.js'
-	}
+		'user-agent': 'node.js',
+		"accept": "application/vnd.github.v3+json"
+	},
 }
+
 
 let body = '';
 
 const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
- 
+	console.log(`statusCode: ${res.statusCode}`)
 
 	res.on('data', d => {
 		body += d.toString('utf8');
@@ -21,7 +27,7 @@ const req = https.request(options, res => {
 	})
 
 	res.on("end", () => {
-		countPushes(body);
+		
 	})
 
 })
@@ -37,7 +43,7 @@ req.end()
 
 function countPushes(data) {
 	data = JSON.parse(data)
-
+	console.log("total: ", data.length)
 	const pushEvents = data.filter(event => event.type === "PushEvent");
 	console.log(pushEvents)
 	console.log(pushEvents.length)
