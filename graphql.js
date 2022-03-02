e1837f53477380580f1da77da3dfa11b001dc9d2
Evelin <teakid21@gmail.com>
2022-03-02 16:56:22 +0100

    Refactor GraphQL user data fetching

--- graphql.js
+++ graphql.js
@@ -1,118 +1,93 @@
 const https = require("https");
 require("dotenv").config()
 
-const user = "evirunurm";
 
-var headers = {
-	'user-agent': 'node.js',
-    "Authorization": `Bearer ${ process.env.GITHUB_TOKEN }`,
-    "Content-Type": "application/json",
-    'Accept': 'application/json',
-};
+// Escaping ENTER's with \ everywhere, except where it's needed (forkCount, stargazerCount)
 
-// var query = `'query':{\
-// 	user(login:'evirunurm'){\
-// 	repositories(first:100, privacy:PUBLIC){\
-// 	totalCount\
-// 	edges {\
-// 	node {\
-// 	forkCount\
-// 	stargazerCount\
-// 	}\
-// 	}\
-// 	}\
-// 	contributionsCollection {\
-// 	contributionCalendar {\
-// 	totalContributions\
-// 	}\
-// 	}\
-// 	followers {\
-// 	totalCount\
-// 	}\
-// 	}\
-// }`
-
-const query = `{\
-user(login:"evirunurm"){\
-repositories(first:100,privacy:PUBLIC){\
-edges{\
-node{\
-forkCount
-stargazerCount\
-}\
-}\
-}\
-contributionsCollection{\
-contributionCalendar{\
-totalContributions\
-}\
-}\
-followers{\
-totalCount\
-}\
-}\
-}`
+const fetchUserData = async (user) => {
+	const headers = {
+		'user-agent': 'Github-Stats',
+	    "Authorization": `Bearer ${ process.env.GITHUB_TOKEN }`,
+	    "Content-Type": "application/json",
+	    'Accept': 'application/json',
+	};
 
 
-// var options = {
-// 	hostname: 'api.github.com',
-//     path: 'https://api.github.com/graphql',
-//     method: 'POST',
-//     headers: headers,
-//     body: JSON.stringify({query})
-// };
+	const query = `{
+	user(login:"${ user }"){
+	repositories(first:100,privacy:PUBLIC,ownerAffiliations:OWNER){
+	edges{
+	node{
+	forkCount
+	stargazerCount
+	}
+	}
+	}
+	contributionsCollection{
+	contributionCalendar{
+	totalContributions
+	}
+	}
+	followers{
+	totalCount
+	}
+	}
+	}`;
 
-// const req = https.request(options, res => {
-
-// 	console.log(res.statusCode);
-// 	let body = "";
-
-// 	res.on("data", (d) => {
-// 		body += d;
-// 	});
-
-// 	res.on("end", () => {
-// 		console.log(body)
-// 	});
-// });
-
-// req.on("error", (error) => {
-// 	console.log("error")
-// 	console.log(error)
-// })
+	const options = {
+		hostname: 'api.github.com',
+	    path: 'https://api.github.com/graphql',
+	    method: 'POST',
+	    body: JSON.stringify({query}),
+	    headers: headers,
+	};
 
 
-// req.end()
+	const request = (resolve, reject) => {
+		const req = https.request(options, res => {
+		let body = "";
 
-console.log(JSON.stringify({query}))
+		res.on("data", (d) => {
+			body += d;
+		});
 
-var options = {
-	hostname: 'api.github.com',
-    path: 'https://api.github.com/graphql',
-    method: 'POST',
-    body: JSON.stringify({query}),
-    headers: headers,
-};
-
-const req = https.request(options, res => {
-
-	console.log(res.statusCode);
-	let body = "";
-
-	res.on("data", (d) => {
-		body += d;
+		res.on("end", () => {
+			resolve(JSON.parse(body));
+		});
 	});
 
-	res.on("end", () => {
-		console.log(body)
-	});
-});
+	req.on("error", (error) => {
+		console.log(error)
+	})
 
-req.on("error", (error) => {
-	console.log("error")
-	console.log(error)
-})
+	req.write(JSON.stringify({query}));
+	req.end()
+	}
 
-req.write(JSON.stringify({query}));
+	const getDataObj = (json) => {
+		let dataObj = {
+			user: user,
+			amountRepos: json.data.user.repositories.edges.length,
+			amountStars: countProperty(json.data.user.repositories.edges, "stargazerCount"),
+			amountForks: countProperty(json.data.user.repositories.edges, "forkCount"),
+			amountFollowers: json.data.user.followers.totalCount,
+			totalContributions: json.data.user.contributionsCollection.contributionCalendar.totalContributions,
+		}
+		return dataObj;
+	} 
 
-req.end()
+	const countProperty = (nodes, property) => {
+		return Object.values(nodes).reduce((t, {node}) => t + node[property], 0)
+	}
+
+	let data = await new Promise( (resolve, reject) => request(resolve, reject));
+	return getDataObj(data);
+}
+
+
+async function foo() {
+	let data = await fetchUserData("evirunurm");
+	console.log(JSON.stringify(data))
+}
+
+foo();
