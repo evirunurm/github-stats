91c5011954f0d2bfbd543af8d7987d7aefc08840
Evelin <teakid21@gmail.com>
2022-03-02 16:58:42 +0100

    Reorganize and refactor

--- fetchUserData.js
+++ fetchUserData.js
@@ -1,38 +1,84 @@
 const https = require("https");
+require("dotenv").config();
 
 const fetchUserData = async (user) => {
+	const headers = {
+		'user-agent': 'Github-Stats',
+	    "Authorization": `Bearer ${ process.env.GITHUB_TOKEN }`,
+	    "Content-Type": "application/json",
+	    'Accept': 'application/json',
+	};
 
-	let body = '';
+
+	const query = `{
+	user(login:"${ user }"){
+	repositories(first:100,privacy:PUBLIC,ownerAffiliations:OWNER){
+	edges{
+	node{
+	forkCount
+	stargazerCount
+	}
+	}
+	}
+	contributionsCollection{
+	contributionCalendar{
+	totalContributions
+	}
+	}
+	followers{
+	totalCount
+	}
+	}
+	}`;
+
 	const options = {
 		hostname: 'api.github.com',
-		path: `/users/${ user }`,
-		method: 'GET',
-		headers: {
-			'user-agent': 'node.js'
-		}
-	}
+	    path: 'https://api.github.com/graphql',
+	    method: 'POST',
+	    body: JSON.stringify({query}),
+	    headers: headers,
+	};
 
-	const request = async (resolve, reject) => {
+
+	const request = (resolve, reject) => {
 		const req = https.request(options, res => {
-	 
-		res.on('data', d => {
-			body += d.toString('utf8');
+			let body = "";
+
+			res.on("data", (d) => {
+				body += d;
+			});
+
+			res.on("end", () => {
+				resolve(JSON.parse(body));
+			});
 		});
 
-		res.on("end", () => {
-			resolve();
+		req.on("error", (error) => {
+			console.log(error);
 		});
-	});
 
-	req.on('error', error => {  
-		console.error(error)
-	});
-
-	req.end();
+		req.write(JSON.stringify({query}));
+		req.end();
 	}
-	
-	await new Promise((r, j) => request(r, j));
-	return body;
+
+	const getDataObj = (json) => {
+		let dataObj = {
+			user: user,
+			amountRepos: json.data.user.repositories.edges.length,
+			amountStars: countProperty(json.data.user.repositories.edges, "stargazerCount"),
+			amountForks: countProperty(json.data.user.repositories.edges, "forkCount"),
+			amountFollowers: json.data.user.followers.totalCount,
+			totalContributions: json.data.user.contributionsCollection.contributionCalendar.totalContributions,
+		}
+		return dataObj;
+	} 
+
+	const countProperty = (nodes, property) => {
+		return Object.values(nodes).reduce((t, {node}) => t + node[property], 0);
+	}
+
+	let data = await new Promise( (resolve, reject) => request(resolve, reject));
+	return getDataObj(data);
 }
 
 exports.fetchUserData = fetchUserData;
--- test.js
+++
@@ -1,2 +0,0 @@
-const fetchers = require("./fetchers");
-
