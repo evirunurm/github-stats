d61efc9eb31285d727f08622141cd49727ab9521
Evelin <teakid21@gmail.com>
2026-03-19 22:50:11 +0100

    chore: add TypeScript, ESLint, and Prettier tooling
    - Add tsconfig.json with strict mode targeting ES2020/CommonJS
    - Add eslint.config.js with typescript-eslint and prettier integration
    - Add .prettierrc with project formatting conventions
    - Add jest.config.js using ts-jest preset for TypeScript tests
    - Update package.json with TypeScript and linting dev dependencies
    - Bump CI to Node 22 and add typecheck + lint steps to test workflow
    
    Closes #19

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+const tseslint = require("typescript-eslint");
+const prettierConfig = require("eslint-config-prettier");
+
+module.exports = tseslint.config(
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  },
+  {
+    ignores: ["node_modules/", "dist/", "jest.config.js", "eslint.config.js"],
+  }
+);
