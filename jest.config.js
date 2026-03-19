7d0363a7546256a6256d89dff0dc77f373190ceb
Evelin <teakid21@gmail.com>
2026-03-19 22:52:11 +0100

    fix: include .test.js files in jest testMatch pattern

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/tests/**/*.test.ts"],
+  testMatch: ["**/tests/**/*.test.ts", "**/tests/**/*.test.js"],
   moduleFileExtensions: ["ts", "js", "json"],
 };
